e0d077103e7deb4b35143afa31c7ef67b14bf6b6
Kaleb Franken <skywrithin@gmail.com>
2022-06-27 12:56:14 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+import Node from './components/Node';
 
 function App() {
+
+  for (let i; i < 15; i++) {
+    let ax = i;
+    for ( let j; j < 15; j++){
+      let ay = j;
+      
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App outline bg-white ">
+      <Node />
     </div>
   );
 }
---
+++ src/components/Node.js
@@ -0,0 +1,13 @@
+
+export default function node() {
+
+  let x = 0;
+  let y = 0;
+
+
+
+
+  return (
+    <div className='w-6 h-6 outline outline-1 bg-gray-400' >{x +','+ y}</div>
+  )
+}
